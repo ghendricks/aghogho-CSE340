553670497cd8946dbce472b225ae9eab39759d4a
aghogho <monorienaghogho@gmail.com>
2025-01-15 15:54:20 +0100

    W03 - Content Delivery and MVC

--- utilities/index.js
+++ utilities/index.js
@@ -59,5 +59,14 @@ Util.buildClassificationGrid = async function(data) {
     return grid
 }
 
+/**
+ * Middleware for Handling Errors
+ * Wrap other function in this for General Error Handling
+ */
+
+Util.handleErrors = fn => (req, res, next) => 
+    Promise.resolve(fn(req, res, next))
+        .catch(next)
+
 
 module.exports = Util
