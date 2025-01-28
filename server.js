95fa99002f613aca3302f8e9182b7584bf07368f
aghogho <monorienaghogho@gmail.com>
2025-01-28 05:16:32 +0100

    w04 - Sessions and messages

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
 const utilities = require("./utilities/")
+const session = require("express-session")
+const pool = require("./database/")
+
+
 
 /* ***********************
  * View Engine and Templates
@@ -20,6 +24,30 @@ const utilities = require("./utilities/")
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout")
+
+
+/***********************
+ * Middleware
+ ***********************/
+app.use(session({
+  store: new (require("connect-pg-simple")(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: "sessionId",
+}))
+
+// Express Message MiddleWare
+app.use(require("connect-flash")())
+app.use(function(req, res, next){
+  res.locals.messages = require("express-messages")(req, res)
+  next()
+})
+
+
 
 
 /* ***********************
