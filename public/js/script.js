57dff4a98330cd5ab3e2aee59237f6c8efc97087
aghogho <monorienaghogho@gmail.com>
2025-01-30 15:37:24 +0100

    W04 - Register or Sign Up

---
+++ public/js/script.js
@@ -0,0 +1,15 @@
+const showHideBtn = document.querySelector("#show-hide");
+showHideBtn.addEventListener("click", () => {
+    const pwInput = document.querySelector("#pwreg");
+    const typeAttr = pwInput.getAttribute("type");
+
+    if (typeAttr === "password") {
+        pwInput.setAttribute("type", "text");
+        showHideBtn.innerHTML = "Hide Password";
+        console.log("Hide password");
+    } else {
+        pwInput.setAttribute("type", "password");
+        showHideBtn.innerHTML = "Show Password";
+        console.log("Show password");
+    }
+});
