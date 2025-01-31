d817fa85fd93247b917cbe91d2ee8abf01df87ae
aghogho <monorienaghogho@gmail.com>
2025-01-31 14:52:05 +0100

    W04 - Servers-side validation

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -10,6 +10,7 @@ async function buildLogin(req, res, next) {
     res.render("./account/login", {
         title: "Login",
         nav,
+        errors: null,
     })
 }
 
@@ -22,6 +23,7 @@ async function buildRegister(req, res, next) {
     res.render("account/register", {
         title: "Register",
         nav,
+        errors: null,
     })
 }
 
